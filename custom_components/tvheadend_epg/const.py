dd613d896c220b68901c84a60aeb51e27ec7994a
rhetesi <66908912+rhetesi@users.noreply.github.com>
2026-01-19 20:47:26 +0100

    Create tvheadend_epg folder and const.py file

---
+++ custom_components/tvheadend_epg/const.py
@@ -0,0 +1,15 @@
+DOMAIN = "tvheadend_epg"
+
+# Config flow keys
+CONF_HOST = "host"
+CONF_PORT = "port"
+CONF_USERNAME = "username"
+CONF_PASSWORD = "password"
+CONF_USE_HTSP = "use_htsp"
+
+# Defaults
+DEFAULT_PORT = 9981
+DEFAULT_UPDATE_INTERVAL = 15  # minutes
+
+# Sensor
+# PLATFORMS = ["sensor"]
